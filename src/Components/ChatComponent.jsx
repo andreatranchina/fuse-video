d82be6a66a32a3c766ed561d86b5280861fe8b32
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-07-14 15:35:00 -0400

    Add image messages

--- src/Components/ChatComponent.jsx
+++ src/Components/ChatComponent.jsx
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
-import 'react-chat-elements/dist/main.css'
-import { ChatItem, MessageBox, Input } from 'react-chat-elements'
-/* import '../Images/chat-avatar.png' */
+import 'react-chat-elements/dist/main.css';
+import { ChatItem, MessageBox, Input } from 'react-chat-elements';
+
+const PhotoMessage = ({ position, text, date, file }) => (
+    <div className={`message-${position}`}>
+        <div>{text}</div>
+        <img src={file.url} alt={text} />
+        <div>{date}</div>
+    </div>
+);
 
 export const ChatBox = () => {
-    
-    // Message storage
     const [input, setInput] = useState('');
-    const [messages, setMessages] = useState([
-
-    ]);
+    const [messages, setMessages] = useState([]);
 
     const handleInput = (value) => {
         setInput(value);
-    }
+    };
 
     const handleSendMessage = () => {
-
         const newMessage = {
             position: 'right',
             type: 'text',
@@ -26,23 +28,71 @@ export const ChatBox = () => {
                 minute: 'numeric',
             }),
         };
-        
+
         setMessages((prevMessages) => [...prevMessages, newMessage]);
         setInput('');
     };
 
+    const handleFileUpload = (event) => {
+        const fileList = event.target.files;
+        const newMessages = Array.from(fileList).map((file) => {
+            if (file && file.type.startsWith('image/')) {
+                const imageUrl = URL.createObjectURL(file);
+                return {
+                    position: 'right',
+                    type: 'photo',
+                    text: file.name,
+                    file: {
+                        name: file.name,
+                        url: imageUrl,
+                    },
+                    date: new Date().toLocaleTimeString('en-US', {
+                        hour: 'numeric',
+                        minute: 'numeric',
+                    }),
+                };
+            }
+            return null;
+        });
+
+        const validNewMessages = newMessages.filter((message) => message !== null);
+
+        setMessages((prevMessages) => [...prevMessages, ...validNewMessages]);
+    };
 
     return (
         <div>
-            <ChatItem
-                /* avatar="chat-avatar.png" */
-                alt="Chat"
-                title="Meeting Chat"
-            />
+            <ChatItem alt="Chat" title="Meeting Chat" />
 
-            {messages.map((message, index) => (
-                <MessageBox {...message} key={index} />
-            ))}
+            {messages.map((message, index) => {
+                if (message.type === 'photo') {
+                    return (
+                        <PhotoMessage
+                            position={message.position}
+                            text={message.text}
+                            date={message.date}
+                            file={message.file}
+                            key={index}
+                        />
+                    );
+                }
+
+                return (
+                    <MessageBox
+                        position={message.position}
+                        type={message.type}
+                        text={message.text}
+                        date={message.date}
+                        file={message.file}
+                        key={index}
+                    />
+                );
+            })}
+
+            <label>
+                Click or Drop Images:
+                <input type="file" accept="image/*" onChange={handleFileUpload} />
+            </label>
 
             <Input
                 placeholder="Send a message"
@@ -51,5 +101,5 @@ export const ChatBox = () => {
                 rightButtons={<button onClick={handleSendMessage}>Send</button>}
             />
         </div>
-    )
-}
+    );
+};
