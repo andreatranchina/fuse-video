e07fa860682beecd4e4fba8daa4db9c5fccf3189
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-07-14 14:21:11 -0400

    Add return statement that renders and updates chat with new messages

--- src/Components/ChatComponent.jsx
+++ src/Components/ChatComponent.jsx
@@ -11,62 +11,27 @@ export const ChatBox = () => {
 
     ]);
 
-    
+    /* const handleInput = () => {
+        
+    } */
 
     return (
         <div>
             <ChatItem
-                avatar="chat-avatar.png"
+                /* avatar="chat-avatar.png" */
                 alt="Chat"
                 title="Meeting Chat"
             />
 
-            <MessageBox
-                position="left"
-                type="text"
-                text="Hello, how can I help you?"
-                dateString="10:00 AM"
-            />
-
-            <MessageBox
-                position={"left"}
-                type={"photo"}
-                title={"Kursat"}
-                data={{
-                    uri: "https://picsum.photos/200/200",
-                }}
-            />
-
-            <MessageBox
-                position="right"
-                type="text"
-                text="Hi, I have a question."
-                dateString="10:05 AM"
-            />
-
-            {/* more messages */}
+            {messages.map((message, index) => (
+                <MessageBox {...message} key={index} />
+            ))}
 
             <Input
-                placeholder="Type your message..."
-                defaultValue=""
-                multiline={false}
-                /*onKeyPress={(event) => {
-                    if (event.key === 'Enter') {
-                        handleSendMessage(event.target.value);
-                        event.target.value = '';
-                    }
-                }}*/
-                rightButtons={
-                    <button
-                        /*onClick={() => {
-                            const messageInput = document.getElementById('messageInput');
-                            handleSendMessage(messageInput.value);
-                            messageInput.value = '';
-                        }}*/
-                    >
-                        Send
-                    </button>
-                }
+                placeholder="Send a message"
+                value={inputMessage}
+                onChange={(event) => setInput(event.target.value)}
+                rightButtons={<button onClick={handleSendMessage}>Send</button>}
             />
         </div>
     )
