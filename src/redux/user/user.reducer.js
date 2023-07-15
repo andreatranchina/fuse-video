1a08b773b0324c431007bb73fe8c8f08b0e71d41
Andrea <Andrea.Tranchina82@gmail.com>
2023-07-15 11:59:05 -0400

    added functionality for logging in and signing up users

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -5,7 +5,7 @@ const defaultUser = {};
 export default function userReducer(state = defaultUser, action) {
     switch (action.type) {
       case GET_USER:
-        return action.user;
+        return action.payload;
       case REMOVE_USER:
         return defaultUser;
       default:
