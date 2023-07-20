6b8ca2e69b0fb4b7fa40e234e51d9ca409bdaa12
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-20 03:17:26 -0400

    create messages side bar template

--- src/Components/messageMenu/GroupMessageIcon.jsx
+++ src/Components/messageMenu/GroupMessageIcon.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
 
 const GroupMessageIcon = () => {
   return (
-    <div>GroupMessageIcon</div>
+    <Box>
+			<Avatar variant='rounded'sx={{width:'70px', height:'70px'}}>
+			</Avatar>
+    </Box>
   )
 }
 
