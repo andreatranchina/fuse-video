5460980a9c54960f80207bdd9a40573ac37025fe
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-07-19 21:37:19 -0400

    Add livestream dispatch and its data.

--- src/Pages/CallPage.jsx
+++ src/Pages/CallPage.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import '../styles/callpage.css';
+import { useDispatch } from 'react-redux';
+import { postLivestreamThunk } from '../redux/livestreams/livestream.actions';
 
 
 import ChatComponent from '../components/ChatComponent';
@@ -9,17 +11,26 @@ const socket = io.connect("http://localhost:3001");
 
 
 function CallPage() {
+
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
   const [showChat, setShowChat] = useState(false);
 
-  const joinRoom = () => {
+  const startRoom = () => {
     if (username !== "" && title !== "" && description !== "" ){
-      
+
+      const livestreamData = {
+        title,
+        description,
+      };
+
+      dispatch(postLivestreamThunk(livestreamData))
       // Get room id from data base
-      socket.emit("join_room", room);
+      socket.emit("start_room", room);
       setShowChat(true);
   }
   }
@@ -33,7 +44,7 @@ function CallPage() {
           <input type="text" placeholder="name" onChange={(e) => {setUsername(e.target.value)}}/>
           <input type="text" placeholder="title" onChange={(e) => { setTitle(e.target.value) }} />
           <input type="text" placeholder="description" onChange={(e) => { setDescription(e.target.value) }} />
-          <button onClick={joinRoom}>Join room</button>
+          <button onClick={startRoom}>Start room</button>
         </div>
       ): (<div>
         <ChatComponent socket={socket} username={username} room={room}/>
