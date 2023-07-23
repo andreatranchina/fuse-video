314c78b546097c60c6cb18cd3ed907ab17c6afec
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-23 03:53:11 -0400

    modal popover within the root layout

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,6 +3,7 @@ import userReducer from "./user/user.reducer"
 import translationReducer from "./translations/translation.reducer"
 import livestreamReducer from "./livestreams/livestream.reducer"
 import messageReducer from "./messages/message.reducer"
+import uiReducer from './ui/ui.reducer'
 
 //function takes in object of all reducers to combine,
 //taking a key value pair 
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
     translations: translationReducer,
     livestreams: livestreamReducer,
     messages: messageReducer,
+    ui: uiReducer,
 })
 
 export default rootReducer
