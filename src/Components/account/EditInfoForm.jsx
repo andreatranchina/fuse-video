b5d7d1b11573355b0bcb0782050381c75ea86df4
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-24 17:37:57 -0400

    structure account settings form aka edit info

--- src/Components/account/EditInfoForm.jsx
+++ src/Components/account/EditInfoForm.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Box, Button, FormControl, FormHelperText, Input, InputLabel, Stack }  from '@mui/material'
+import { Box, Button, FormControl, FormHelperText, Input, InputLabel, Stack, TextField }  from '@mui/material'
 import { editFormField, submitSuccess, submitFail, flagErrors } from '../../redux/forms/forms.actions';
 import MobileValidation from '../validators/MobileValidation';
 import PasswordValidtator from '../validators/PasswordValidator';
+import { editAccountThunk } from '../../redux/user/user.actions';
+import { parsePhoneNumber, isValidNumber, format } from 'libphonenumber-js';
 
 const EditInfoForm = () => {
 
   const dispatch = useDispatch();
 
   const isEditingAccount = useSelector((state) => !!state.user.isEditingAccount)
-  const { firstName, lastName, email, isSuccess, errors } = useSelector((state) => state.forms)
-  
+  const { firstName, lastName, email, isSuccess, errors, mobile, country } = useSelector((state) => state.forms)
+  const userId = useSelector((state) => state.user.defaultUser?.id)
 
   //need a selector to fetch all the data from the user prior to the form uploading
 
@@ -35,72 +37,112 @@ const EditInfoForm = () => {
     
     if (!isValidEmail(email)){
       dispatch(flagErrors({ email: 'Invalid Email'})) 
+      console.log('invalid email')
     } 
 
-    const hasErrors = Object.keys(errors).length > 0;
+    const mobileError = errors.mobile;
 
-    if (!hasErrors){
-      submitSuccess();
+  if (mobile.trim() === '') {
+    // If the mobile number is empty (only whitespace), dispatch an error message for the mobile field
+    dispatch(flagErrors({ mobile: 'Please input a mobile number without area code' }));
+  } else if (mobileError) {
+    // If there was already an error for the mobile field, display that error message
+    console.log(mobileError);
+  } else {
+    try {
+      // Parse the phone number
+      const mobileNumberInstance = parsePhoneNumber(mobile, country);
+      console.log(mobileNumberInstance.number);
+
+      // Check if the phone number is valid, using the isValidNumber method from react-phone-number-input
+      const isValidMobile = mobileNumberInstance.number ? isValidNumber(mobileNumberInstance.number) : false;
+      console.log(isValidMobile);
+
+      // Format the phone number if it's valid
+      if (isValidMobile) {
+        dispatch(editFormField('mobile', mobileNumberInstance.number));
+      } else {
+        // Handle the case when the phone number is not valid
+        dispatch(flagErrors({ mobile: 'Invalid mobile number' }));
+      }
+    } catch (error) {
+      // Handle any exceptions that occur during phone number parsing
+      console.error('Error parsing phone number:', error);
+      dispatch(flagErrors({ mobile: 'Error parsing phone number' }));
+    }
+  }
+
+    const areErrorsEmpty = Object.values(errors).every((value) => value === '');
+
+    console.log(areErrorsEmpty)
+
+    if (areErrorsEmpty){
+      dispatch(submitSuccess());
+      const editedAccount = {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        mobile: mobile,
+        country: country
+      }
+      console.log(editedAccount);
+      dispatch(editAccountThunk(6, editedAccount));
       //editUserInfo(firstName, lastName, email, mobile, errors, isSuccess);
     } else {
-      submitFail();
+      console.log('failed');
+      dispatch(submitFail());
     }
   }
 
   return (
     <Box 
     component='form'
-    autoComplete='off'>
-    <Stack>
-      <FormControl
-        variant='outlined'>
-        <InputLabel htmlFor='component-first'>First Name
-        </InputLabel>
-        <Input
-          id='component-first'
-          placeholder={firstName}
-          onChange={(e) => handleChange('firstName',e.target.value)}
+    autoComplete='off'
+    display={'flex'}
+    justifyContent={'center'} >
+    <Stack spacing={4} sx={{width:'400px'}}>
+     <TextField
+          label='First Name'
+          variant={isSuccess ? 'filled' : 'standard'}
+          value={firstName}
+          onChange={(e) => handleChange('firstName', e.target.value)}
+          helperText={errors.firstName}
+          error={!!errors.firstName}
         />
-        {errors.firstName && <FormHelperText>{errors.firstName}</FormHelperText>}
-      </FormControl>
-      <FormControl
-        variant='outlined'>
-        <InputLabel htmlFor='component-first'>Last Name
-        </InputLabel>
-        <Input
-          id='component-last'
-          placeholder={lastName}
-          onChange={(e) => handleChange('lastName',e.target.value)}
-        />
-        {errors.lastName && <FormHelperText>{errors.lastName}</FormHelperText>}
-      </FormControl>
-      <FormControl
-        variant={isSuccess ? 'filled' : 'outlined'}
-        error={!!errors.name}
-      >
-        <InputLabel htmlFor='component-email'>Email
-        </InputLabel>
-        <Input
-          id='component-email'
-          placeholder={email}
-          onChange={(e) => handleChange('email',e.target.value)}
-        />
-        {errors.email && <FormHelperText>{errors.email}</FormHelperText>}
-      </FormControl>
-      {/* <FormControl>
-        <InputLabel htmlFor="password-input">Password</InputLabel>
-        <PasswordValidtator/>
-        {errors.password && <FormHelperText error>{errors.password}</FormHelperText>}
-      </FormControl> */}
-      <FormControl
-        variant={isSuccess ? 'filled' : 'outlined'}
-        error={!!errors.mobile}
-      >
-        <InputLabel htmlFor='component-mobile'>Mobile Number
-        </InputLabel>
-        <MobileValidation/>
-        {errors.mobile && <FormHelperText>{errors.mobile}</FormHelperText>}
-      </FormControl>
+      <TextField
+        label="Last Name"
+        variant={isSuccess ? 'filled' : 'standard'}
+        value={lastName}
+        onChange={(e) => handleChange('lastName',e.target.value)}
+        helperText={errors.lastName}
+        error={!!errors.lastName}
+      />
+      <TextField
+        label="Email"
+        variant={isSuccess ? 'filled' : 'standard'}
+        value={email}
+        onChange={(e) => handleChange('email',e.target.value)}
+        helperText={errors.email}
+        error={!!errors.email}
+      />
+       <TextField
+            id='component-mobile'
+            label='Mobile Number'
+            variant={isSuccess ? 'filled' : 'standard'}
+            value={mobile}
+            onChange={(e) => handleChange('mobile', e.target.value)}
+            helperText={errors.mobile}
+            error={!!errors.mobile}
+            InputProps={{
+              inputComponent: MobileValidation, // Use MobileValidation component as the input
+            }}
+            sx={{
+    "& label": { // Apply styles to the label element within the TextField
+      transform: 'translate(10px,5px)',
+      fontSize:'13px' // Adjust the value based on your desired downward movement
+    },
+  }}
+          />
         <Button onClick={handleSave}>
          "Save"
         </Button>
