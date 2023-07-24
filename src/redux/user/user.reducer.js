8bff70bde6e6e03c8059152ccab4be05f534a829
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-24 17:38:45 -0400

    create redux for editing account information and storing temporary form data

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,9 @@
-import { GET_USER, REMOVE_USER, EDIT_PROFILE, EDIT_ACCOUNT } from "./user.types";
+import { GET_USER, REMOVE_USER, EDIT_ACCOUNT, EDIT_STATUS } from "./user.types";
 
 
 export const INITIAL_USER_STATE = {
   defaultUser: {},
-  isEditingProfile: false,
-  isEditingAccount: false
+  editedProfile:[],
 }
 
 export default function userReducer(state = INITIAL_USER_STATE, action) {
@@ -15,15 +14,19 @@ export default function userReducer(state = INITIAL_USER_STATE, action) {
         return {
         ...INITIAL_USER_STATE
       };
-      case EDIT_PROFILE:
-        return {
-          ...state, isEditingProfile: !state.isEditingProfile
-      }
+      //edit the profile information
       case EDIT_ACCOUNT:
+      return {
+        ...state,
+        defaultUser: {
+          ...state.defaultUser,
+          ...action.payload,
+        },
+      };
+      case EDIT_STATUS:
         return {
           ...state, isEditingAccount: !state.isEditingAccount
       }
-      // case EDIT_PROFILE
       default:
         return state;
     }
