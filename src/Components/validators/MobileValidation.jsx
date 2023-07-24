3004e35889246f82eb9594d57bec1ec26c334913
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-24 17:40:33 -0400

    mobile phone validation for edit info form

--- src/Components/validators/MobileValidation.jsx
+++ src/Components/validators/MobileValidation.jsx
@@ -1,45 +1,31 @@
 import React, { useState } from 'react';
-import { parsePhoneNumberFromString, isValidNumber, format } from 'libphonenumber-js';
-import { editFormField, flagErrors } from '../../redux/forms/forms.actions';
 import PhoneInput from 'react-phone-number-input'
 import { useDispatch, useSelector } from 'react-redux'
+import { editFormField } from '../../redux/forms/forms.actions';
 import 'react-phone-number-input/style.css'
 
 const MobileValidation = () => {
   // const [mobileToValidate, setMobileToValidate] = useState('');
   // const [isValidMobileNumber, setIsValidMobileNumber] = useState(false);
   const dispatch = useDispatch()
-  const mobile = useSelector((state) => state.forms.mobile)
+  const { mobile, country } = useSelector((state) => state.forms)
   const userMobile = useSelector((state) => state.user.defaultUser?.mobileNumber)
 
-  const handlePhoneNumberChange = (event) => {
-    const newMobileNumber = event.target.value;
-    // setMobileToValidate(newMobileNumber);
-
-    // Parse the phone number
-    const mobileNumberInstance = parsePhoneNumberFromString(newMobileNumber);
-
-    // Check if the phone number is valid, using the isValidNumber methom react-phone-number-input
-    const isValid = mobileNumberInstance ? isValidNumber(mobileNumberInstance) : false;
-    // setIsValidMobileNumber(isValid);
-
-    // Format the phone number if it's valid
-    if (isValid) {
-      const formatted = format(mobileNumberInstance, 'E.164');
-      dispatch(editFormField('mobile',formatted))
-    } else {
-     //get the phone number already in the account
-    //  dispatch(editFormField('mobile',userMobile))
-      dispatch(flagErrors({ mobile: 'Invalid mobile number'}))
-    }
+  const handlePhoneNumberChange = (fieldName, newValue) => {
+    console.log(newValue);
+    dispatch(editFormField(fieldName, newValue));
   };
 
   return (
     <>
     <PhoneInput
           id='component-mobile'
-          placeholder={mobile}
-          onChange={(e) => handlePhoneNumberChange(e)}
+          value={mobile}
+          // onChange={(e) => handlePhoneNumberChange(e)}
+          onChange={(mobileNumber) => handlePhoneNumberChange('mobile',mobileNumber)}
+          onCountryChange={(country) => handlePhoneNumberChange('country',country)}
+          style={{height:'60px', transform:'translate(13px,10px)', width: '90%'}}
+          specialLabel='Mobile Phone'
         />
     </>
   );
