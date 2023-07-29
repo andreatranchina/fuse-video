e102802cfe218793992a8c61f9c2727bf21e0c2f
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-29 06:06:46 -0400

    merge in main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,68 +33,76 @@ const Home = () => {
   };
 
   const { mode } = useThemeContext();
-  const isSmallScreen = useMediaQuery('(max-width: 900px');
+  const isSmallScreen = useMediaQuery('(max-width: 900px)');
   const isMobileScreen = useMediaQuery('(max-width: 420px)');
 
-  return (
-    <>
-    {isMobileScreen ? (<Box id={mode === 'light' ? 'mobile-home-light' : 'mobile-home-dark'} sx={{ display: 'flex', justifyContent: 'center', marginTop: user? '50px' : ''}}>
-      <Stack sx={{alignItems: 'center'}} spacing={3}>
+ return (
+  <Box>
+    {isMobileScreen ? (
+      <Box
+        id={mode === 'light' ? 'mobile-home-light' : 'mobile-home-dark'}
+        sx={{ display: 'flex', justifyContent: 'center', marginTop: user ? '50px' : '' }}
+      >
+        <Stack sx={{ alignItems: 'center' }} spacing={3}>
           <Slogan />
-          <SubSlogan/>
-          <MobileAvatarSlogan/>
+          <SubSlogan />
+          <MobileAvatarSlogan />
           <MobileDescription />
-          <MobileSwiper/>
-      </Stack>
-        {/* {user ? <h1>Welcome, {user.firstName + " " + user.lastName}</h1> : <h1>Please signup or login!</h1>}
-        <button style={{ height: '30px' }} onClick={handleLogout}>Logout</button> */}
-    </Box>) 
-    
-    : isSmallScreen ? (<Box id={mode === 'light' ? 'home-light' : 'home-dark'} sx={{ display: 'flex', justifyContent: 'center', marginTop: user? '50px' : '' }}>
-      <Stack sx={{ width: '70%' }}>
-        <Stack direction='row' sx={{ width: '100vw' }}>
+          <MobileSwiper />
+        </Stack>
+      </Box>
+    ) : isSmallScreen ? (
+      <Box
+        id={mode === 'light' ? 'home-light' : 'home-dark'}
+        sx={{ display: 'flex', justifyContent: 'center', marginTop: user ? '50px' : '' }}
+      >
+        <Stack sx={{ width: '70%' }}>
+          <Stack direction='row' sx={{ width: '100vw' }}>
             <Grid container direction='column' sx={{ height: '100%' }}>
-              <Grid item xs={6} sx={{display: 'flex', width:'100%'}}>
+              <Grid item xs={6} sx={{ display: 'flex', width: '100%' }}>
                 <Slogan />
               </Grid>
             </Grid>
-          <Stack sx={{ width: '30%', alignItems:'centers'}}>
-          <Box>
-            <HomeImagesContainer />
-            </Box>
+            <Stack sx={{ width: '30%', alignItems: 'centers' }}>
+              <Box>
+                <HomeImagesContainer />
+              </Box>
+            </Stack>
           </Stack>
-          </Stack>
-          
-           <Box sx={{ width:'100%', height:'100%', alignItems:'center', transform: 'translate(-100px,-60px)', zIndex:2}}>
-              <Description />
-            </Box>
+
+          <Box sx={{ width: '100%', height: '100%', alignItems: 'center', transform: 'translate(-100px,-60px)', zIndex: 2 }}>
+            <Description />
+          </Box>
         </Stack>
-        {/* {user ? <h1>Welcome, {user.firstName + " " + user.lastName}</h1> : <h1>Please signup or login!</h1>}
-        <button style={{ height: '30px' }} onClick={handleLogout}>Logout</button> */}
-      </Box>) 
-    
-    
-    : (<Box id={mode === 'light' ? 'home-light' : 'home-dark'} sx={{ display: 'flex', justifyContent: 'center', marginTop: user? '50px' : '' }}>
+      </Box>
+    ) : (
+      <Box
+        id={mode === 'light' ? 'home-light' : 'home-dark'}
+        sx={{ display: 'flex', justifyContent: 'center', marginTop: user ? '50px' : '' }}
+      >
         <Stack direction='row' sx={{ width: '100vw' }}>
           <Stack sx={{ width: '70%' }}>
             <Grid container direction='column' sx={{ height: '100%' }}>
-              <Grid item xs={6} sx={{display: 'flex', width:'100%'}}>
+              <Grid item xs={6} sx={{ display: 'flex', width: '100%' }}>
                 <Slogan />
               </Grid>
-              <Grid item xs={6} sx={{ width:'100%', height:'100%', alignItems:'center', transform:'translateY(-50px)', zIndex:2}}>
+              <Grid item xs={6} sx={{ width: '100%', height: '100%', alignItems: 'center', transform: 'translateY(-50px)', zIndex: 2 }}>
                 <Description />
               </Grid>
             </Grid>
           </Stack>
-          <Stack sx={{ width: '30%', alignItems:'centers'}}>
-          <Box>
-            <HomeImagesContainer />
+          <Stack sx={{ width: '30%', alignItems: 'centers' }}>
+            <Box>
+              <HomeImagesContainer />
             </Box>
           </Stack>
         </Stack>
-      <button onClick={handleExploreClick}>Explore</button>
-    </Box>
-  )
-}
+        <button onClick={handleExploreClick}>Explore</button>
+      </Box>
+    )}
+  </Box>
+);
+
+    }
 
 export default Home
