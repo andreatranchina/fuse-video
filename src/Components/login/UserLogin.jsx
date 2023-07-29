6a2cf8bafe3919c012265deccceb51cfa4fa157d
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-29 05:50:51 -0400

    add box mui wrapper

--- src/Components/login/UserLogin.jsx
+++ src/Components/login/UserLogin.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Box } from '@mui/material'
 import { NavLink } from 'react-router-dom'
 import '../../styles/navbar.css'
 
 const UserLogin = () => {
   return (
-    <div className="login-navlinks">
+    <Box className="login-navlinks" >
         <NavLink className="login-navlink" to="/login" >LOGIN</NavLink>
         <NavLink  className="login-navlink"  to="/signup">SIGN UP</NavLink>
-    </div>
+    </Box>
   )
 }
 
