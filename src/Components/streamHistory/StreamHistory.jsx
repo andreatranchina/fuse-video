c85a462dd45327f31d11eee69ed25a104d1bd28d
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-07-30 07:47:20 -0400

    add logic for fetching streams

--- src/Components/streamHistory/StreamHistory.jsx
+++ src/Components/streamHistory/StreamHistory.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import VideoThumbnail from './VideoThumbnail'
+import { useSelector } from 'react-redux'
 
-const StreamHistory = () => {
+const StreamHistory = ({viewUserId}) => {
+  const loggedInUserId = useSelector((state) => state.user.defaultUser?.id)
+  const isOwnProfile = loggedInUserId === Number(viewUserId); 
+  
   return (
     <Grid container >
       <Grid item/>
